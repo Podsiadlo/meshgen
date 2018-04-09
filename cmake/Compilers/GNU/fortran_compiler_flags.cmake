12b87cc263725a5c2265edc0e32db24cd45717c6
Albert Oliver <albert.oliver@ulpgc.es>
2018-04-09 18:34:51 +0100

    IMP: Modern approach to add compiler options in CMake

--- cmake/Compilers/GNU/fortran_compiler_flags.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -std=f2008 -fall-intrinsics -W -Wall -Wimplicit-interface -pedantic-errors -frealloc-lhs -Wuse-without-only")
-set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -O3 -march=native -funroll-loops")
-set(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} -Og -fcheck=all -Wunderflow -fbacktrace -fbounds-check -g -finit-real=nan")
-
